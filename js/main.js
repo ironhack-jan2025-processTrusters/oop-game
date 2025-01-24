a90172113781522e552a42e938da6117da1d9780
Luis Junco <38411763+ludovico-wd@users.noreply.github.com>
2025-01-24 14:05:26 +0100

    implement functionality for obstacles (create obstacles + move)

--- js/main.js
+++ js/main.js
@@ -2,19 +2,19 @@
 
 class Player {
     constructor() {
-        this.positionX = 10;
-        this.positionY = 0;
         this.width = 20;
         this.height = 10;
+        this.positionX = 10;
+        this.positionY = 0;
 
         this.playerElm = document.getElementById("player");
         this.updateUI();
     }
     updateUI(){
+        this.playerElm.style.width =  this.width + "vw";
+        this.playerElm.style.height =  this.height + "vh";
         this.playerElm.style.left = this.positionX + "vw";
         this.playerElm.style.bottom = this.positionY + "vh";
-        this.playerElm.style.width =  this.width + "vw";
-        this.playerElm.style.height =  this.height + "vh"; 
     }
     moveLeft() {
         this.positionX--;
@@ -27,7 +27,56 @@ class Player {
 }
 
 
+class Obstacle {
+    constructor(){
+        this.width = 20;
+        this.height = 20;
+        this.positionX = Math.floor(Math.random() * (100 - this.width + 1)); // random number between 0 and (100 - width)        
+        this.positionY = 100;
+
+        this.createDomElement();
+    }
+    createDomElement () {
+        // step1: create the element:
+        this.obstacleElm = document.createElement("div");
+
+        // step2: add content or modify
+        this.obstacleElm.className = "obstacle";
+        this.obstacleElm.style.width = this.width + "vw";
+        this.obstacleElm.style.height = this.height + "vh";
+        this.obstacleElm.style.left = this.positionX + "vw";
+        this.obstacleElm.style.bottom = this.positionY + "vh";
+
+        //step3: append to the dom: `parentElm.appendChild()`
+        const parentElm = document.getElementById("board");
+        parentElm.appendChild(this.obstacleElm);
+    }
+    moveDown() {
+        this.positionY--;
+        this.obstacleElm.style.bottom = this.positionY + "vh";
+    }
+}
+
+
+
 const player = new Player();
+
+const obstaclesArr = []; // will store instances of the class Obstacle
+
+
+// create obstacles
+setInterval(() => {
+    const newObstacle = new Obstacle();
+    obstaclesArr.push(newObstacle);
+}, 3000);
+
+// move obstacles
+setInterval(() => {
+    obstaclesArr.forEach((obstacleInstance, i, arr) => {
+        obstacleInstance.moveDown();
+    });
+}, 30);
+
 
 
 document.addEventListener("keydown", (event) => {
