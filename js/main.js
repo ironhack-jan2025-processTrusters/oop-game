650051a30a27668125f753657537fded71797cc7
Luis Junco <38411763+ludovico-wd@users.noreply.github.com>
2025-01-24 10:59:04 +0100

    implement player functionality

--- js/main.js
+++ js/main.js
@@ -1,1 +1,40 @@
-console.log("hello world");
+
+
+class Player {
+    constructor() {
+        this.positionX = 10;
+        this.positionY = 0;
+        this.width = 20;
+        this.height = 10;
+
+        this.playerElm = document.getElementById("player");
+        this.updateUI();
+    }
+    updateUI(){
+        this.playerElm.style.left = this.positionX + "vw";
+        this.playerElm.style.bottom = this.positionY + "vh";
+        this.playerElm.style.width =  this.width + "vw";
+        this.playerElm.style.height =  this.height + "vh"; 
+    }
+    moveLeft() {
+        this.positionX--;
+        this.updateUI();
+    }
+    moveRight() {
+        this.positionX++;
+        this.updateUI();
+    }
+}
+
+
+const player = new Player();
+
+
+document.addEventListener("keydown", (event) => {
+    if (event.code === 'ArrowLeft') {
+        player.moveLeft();
+    } else if (event.code === 'ArrowRight') {
+        player.moveRight();
+    }
+});
+
